71ea27121c04e0c9101ac69d573a3640859222e0
Your Name <yan.vi193@gmail.com>
2022-01-25 14:58:02 +0100

    genera-e-stampa-10-indirizzi-email

--- js/myscript.js
+++ js/myscript.js
@@ -11,7 +11,22 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 const app = new Vue ({
     el: "#myEmailApp",
     data: {
-        
+        emailContainer: []
     },
+    methods:{
 
+    },
+    created(){
+        for (let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+               // console.log(response.data.response);
+                this.emailContainer.push(response.data.response);
+            })
+            .catch((error) => {
+                console.log(error);
+            }
+            )
+        }
+    }
 })
