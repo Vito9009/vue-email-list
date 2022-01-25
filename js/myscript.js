603948763ee6e8c41d4cee584c0bb5071349bb6f
Your Name <yan.vi193@gmail.com>
2022-01-25 14:41:56 +0100

    creazione-file

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+/*
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+const app = new Vue ({
+    el: "#myEmailApp",
+    data: {
+        
+    },
+
+})
