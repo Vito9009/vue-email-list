4b7ee44de14e50fb117f2f90df0d39b7cccc3c80
Your Name <yan.vi193@gmail.com>
2022-01-25 15:41:15 +0100

    fix

--- js/myscript.js
+++ js/myscript.js
@@ -16,7 +16,7 @@ const app = new Vue ({
     methods:{
 
     },
-    created(){
+    mounted(){
         for (let i = 0; i < 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((response) => {
